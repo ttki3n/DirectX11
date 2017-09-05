eb6c114dc81b3547219d020190118e171b0dd36b
Kien Tran Trung <ttki3n@gmail.com>
2017-09-05 11:22:17 +0700

    Add a simple vertex shader and pixel shader

---
+++ src/shaders/colorVS.hlsl
@@ -0,0 +1,32 @@
+// global 
+cbuffer MatrixBuffer
+{
+	matrix worldMatrix;
+	matrix viewMatrix;
+	matrix projectionMatrix;
+};
+
+struct VertexInputType
+{
+	float4 position: POSITION;
+	float4 color: COLOR;
+};
+
+struct PixelInputType
+{
+	float4 position: SV_POSITION;
+	float4 color: COLOR;
+};
+
+PixelInputType ColorVS(VertexInputType input)
+{
+	PixelInputType output;
+	input.position.w = 1.0f;
+
+	output.position = mul(input.position, worldMatrix);
+	output.position = mul(output.position, viewMatrix);
+	output.position = mul(output.position, projectionMatrix);
+
+	output.color = input.color;
+	return output;
+}
